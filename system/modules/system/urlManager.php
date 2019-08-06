fb5bfa4f570dd47ac11a2b7ce0695b55be30f5a3
Onur MIO <onurmio98@gmail.com>
2019-08-06 23:03:58 +0300

    Redirect function is added.

--- system/modules/system/urlManager.php
+++ system/modules/system/urlManager.php
@@ -1,17 +1,22 @@
 <?php
 namespace System\Modules;
 
-class urlManagerModule{
+class urlManagerModule
+{
+    public function __construct(string $url)
+    {
+        $this->url = $url;
+    }
 
-  public function __construct(string $url)
-  {
-    $this->url = $url;
-  }
+    public function E404()
+    {
+        exit(header("Location:" . $this->url . '/404'));
+    }
 
-  public function E404()
-  {
-  	exit(header("Location:" . $this->url . '/404'));
-  }
-
+    public function redirect(string $url, bool $redirect = true)
+    {
+        if ($redirect) {
+            exit(header("Location:".$url));
+        }
+    }
 }
-?>
