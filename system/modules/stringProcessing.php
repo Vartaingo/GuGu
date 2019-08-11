e5abc1091d4a583e2aa978ab46e8af694f9e38d8
Onur MIO <onurmio98@gmail.com>
2019-08-11 04:18:04 +0300

    createToken function is updated.

--- system/modules/stringProcessing.php
+++ system/modules/stringProcessing.php
@@ -18,13 +18,13 @@ class stringProcessingModule
         return openssl_decrypt($data, "aes-256-cbc", hash("sha256", $key), 0, substr(hash("sha256", "35DFD4A32ECE54400800FFC275F87ADC"), 0, 16));
     }
 
-    public function createToken(array $params)
+    public function createToken(array $params, bool $temp = false)
     {
-        $return_value = "";
-
+        $return_value = null;
+        ($temp) ? array_push($params, date("d/m/Y")) : null ;
         foreach ($params as $param) {
-            $return_value .= $this->encryptWithOutKey($param);
+            $return_value .= $param;
         }
-        return $return_value;
+        return $this->encryptWithOutKey($return_value);
     }
 }
